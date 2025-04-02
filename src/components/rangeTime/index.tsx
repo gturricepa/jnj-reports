2d41f93e8abfdb8e7dce151a21f7b5f727a15154
gturricepa <gturri@cepasafedrive.com>
2025-04-02 11:16:05 -0300

    remove unsed vars

--- src/components/rangeTime/index.tsx
+++ src/components/rangeTime/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Tooltip } from "antd";
 import { CalendarOutlined } from "@ant-design/icons";
 import * as S from "./styles";
